45b4a6b6af769b757e77f2d3dad94ee8f8993017
bouquet1 <bukuleta@gmail.com>
2023-09-14 19:36:10 -0700

    4 passes

--- lib/dog.py
+++ lib/dog.py
@@ -4,7 +4,8 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, id = None):
+        self.id = id
         self.name = name
         self.breed = breed
     
@@ -19,4 +20,26 @@ class Dog:
     """
         CURSOR.execute(sql)
 
-Dog.create_table()
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS dogs
+    """
+        CURSOR.execute(sql)
+
+
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)    
+    """
+        #persist 
+        CURSOR.execute(sql, (self.name, self.breed))
+        CONN.commit()
+    
+    @classmethod
+    def create(cls, name, breed):
+        pet = cls(name, breed)
+        pet.save()
+        return pet
+
